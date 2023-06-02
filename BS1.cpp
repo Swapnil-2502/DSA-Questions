33d5cf8fa0ce437ed95d0b0201e646379b6d7890
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-06-02 23:35:37 +0530

    BS1 iterative & Recursive

--- BS1.cpp
+++ BS1.cpp
@@ -19,3 +19,28 @@ int search(vector<int>& nums, int target) {
 }
 //TC = O(logn)
 //SC = O(1)
+
+//Recursion 
+int binarySearch(vector<int>& nums, int target, int low, int high){
+    if(low > high) return -1;
+
+    int mid = (low+high)/2;
+
+    if(target == nums[mid]) return mid;
+
+    else if(target > nums[mid]) return binarySearch(nums,target,mid+1,high);
+
+    else return binarySearch(nums,target,low,mid-1);
+}   
+
+int search(vector<int>& nums, int target) {
+    int n = nums.size();
+    int low = 0;
+    int high = n-1;
+
+    int ans = binarySearch(nums,target,low,high);
+
+    return ans;
+}
+// //TC = O(logn)
+// //SC = O(logn) for auxiliary space
