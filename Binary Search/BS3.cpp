1a0e2f84808bcc83482591ff54bac338d9209591
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-08-06 20:36:06 +0530

    First and last Occ added & README.md updated

---
+++ Binary Search/BS3.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h> 
+int lowerBound(vector<int> arr, int n, int x) {
+	
+	int ans = n;
+
+	int low = 0;
+	int high = n-1;
+
+	while(low <= high){
+		int mid = (low + high)/2;
+
+        //May be an ans
+		if(arr[mid] >= x){
+			ans = mid;
+            
+			high = mid-1; //Look for more small index in left 
+		}
+		else low = mid+1; //Look for right 
+	}
+
+	return ans;
+}
+int upperBound(vector<int> &arr, int n, int x){
+	
+	int ans = n;
+
+	int low = 0;
+	int high = n-1;
+
+	while(low <= high){
+		int mid = (low + high)/2;
+
+		if(arr[mid] > x){
+			ans = mid;
+			high = mid-1;
+		}
+		else low = mid+1;
+	}
+
+	return ans;
+}
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    //Brute
+    // int first = -1;
+    // int last = -1;
+
+    // for(int i=0;i<n;i++){
+    //     if(arr[i] == k){
+    //         if(first == -1){
+    //             first = i;
+    //             last = i;
+    //         }
+    //         else if(first != -1) last = i;
+    //     }
+    // }
+    // return {first,last};
+    //SC = O(1);
+    //TC = O(N);
+
+    //Improve TC = O(N)
+    //Apply Binary Search O(logN);
+    int lb = lowerBound(arr,n,k);
+    if( lb == n || arr[lb] != k) return {-1,-1};
+    return {lb,upperBound(arr,n,k)-1};
+    
+    //TC = O(logN);
+    //SC = O(1);
+}
