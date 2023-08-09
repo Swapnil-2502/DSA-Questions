30ba82fd47645edecce4fa3687b11cb0153a636f
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-08-09 23:41:08 +0530

    BS5 added

---
+++ Binary Search/BS5.cpp
@@ -0,0 +1,41 @@
+bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
+    
+    int n = arr.size();
+    int low = 0;
+    int high = n-1;
+
+    while(low<=high){
+
+        int mid = (low+high)/2;
+
+        if(arr[mid]==k) return true;
+
+        if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+            low++;
+            high--;
+            continue;
+        }
+
+        if(arr[low]<=arr[mid]){ //right half is not sorted
+
+            if(arr[low]<=k && k<=arr[mid]){ //Means k is in left half
+                high = mid-1;
+            }   
+            else{
+                low = mid+1;
+            }
+
+        }else{  //left half is not sorted
+
+            if(arr[mid]<=k && k<=arr[high]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+    }
+    return false;
+    //TC = O(logN);
+    //Sc = O(1);
+}
